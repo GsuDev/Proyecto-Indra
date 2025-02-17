e014990a6db0bf3df3453260c881820fcc899196
GsuDev <156043286+GsuDev@users.noreply.github.com>
2025-02-17 23:45:46 +0100

    Reinicio (insercion de los archivos)

---
+++ Programacion/practicasIndra/src/ecoEvent/Usuario.kt
@@ -0,0 +1,78 @@
+package ecoEvent
+
+
+class Usuario(
+    private var nombre: String
+    private var ap1: String
+    private var ap2: String?
+    private var correo: String
+    private var contrasena: String
+    private var tlf: Int
+) {
+
+    companion object {
+        var cont = 1
+    }
+    private var id: Int = cont++
+
+
+    // Getters
+    fun getId(): Int {
+        return id
+    }
+
+    fun getNombre(): String {
+        return nombre
+    }
+
+    fun getAp1(): String {
+        return ap1
+    }
+
+    fun getAp2(): String? {
+        return ap2
+    }
+
+    fun getCorreo(): String {
+        return correo
+    }
+
+    fun getContrasena(): String {
+        return contrasena
+    }
+
+    fun getTlf(): Int? {
+        return tlf
+    }
+
+    // Setters
+    fun setId(id: Int) {
+        this.id = id
+    }
+
+    fun setNombre(nombre: String) {
+        this.nombre = nombre
+    }
+
+    fun setAp1(ap1: String) {
+        this.ap1 = ap1
+    }
+
+    fun setAp2(ap2: String?) {
+        this.ap2 = ap2
+    }
+
+    fun setCorreo(correo: String) {
+        this.correo = correo
+    }
+
+    fun setContrasena(contrasena: String) {
+        this.contrasena = contrasena
+    }
+
+    fun setTlf(tlf: Int?) {
+        this.tlf = tlf
+    }
+}
+
+
